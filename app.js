96f4699e533c2bdfbc4e3bc950f0a5760380e0db
Jesse <jesus.villarreal02@utrgv.edu>
2017-08-11 09:17:12 -0500

    Sessions and cookies
    Testing sessions and cookies
    added Bootstrap for Flash usage.

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var session = require('express-session');
+var formidable = require('formidable');
 var fortune = require('./lib/fortune.js');
-
+var credentials = require('./credentials.js');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,7 +16,17 @@ var about = require('./routes/about');
 
 
 var app = express();
-var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+var handlebars = require('express3-handlebars').create({
+    defaultLayout: 'main',
+    helpers: {
+        section: function (name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -34,20 +46,185 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/users', users);
 //app.use('/about', about);
 
+function getWeatherData() {
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+/// SETS UP COOKIE Password and Session key
+//////////////////////////////////////////////////////////////////////////////////////
+app.use(cookieParser(credentials.cookieSecret));
+app.use(session({
+    secret: credentials.cookieSecret,
+    resave: true,
+    saveUninitialized: true,
+    })
+);
+//////////////////////////////////////////////////////////////////////////////////////
+app.use(function (req, res, next) {
+    if (!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weather = getWeatherData();
+    next();
+});
+
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
     next();
 });
 
+app.use(function (req, res, next) {
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+    //if there's a flash message, transfer it to the context, then clear it
+});
+
+
+/////////////////////////////////////////////////////////////////////////
+//NOTE: Routes and FORM POST
+/////////////////////////////////////////////////////////////////////////
+app.get('/newsletter', function (req, res) {
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+
+
+///NOTE: Flash Messages for Newsletter Sign Up
+app.post('/newsletter', function (req, res) {
+    var name = req.body.name || '', email = req.body.email || '';
+    //input validation
+    if (!email.match(VALID_EMAIL_REGEX)) {
+        if (req.xhr) return res.json({ error: 'Invalid name email address' });
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was not valid.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+    new NewsletterSignup({ name: name, email: email }).save(function (err) {
+        if (err) {
+            if (req.xhr) return res.json({ error: 'Database Error.' });
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Database Error!',
+                message: 'There was a database error; please try again later.',
+            };
+            return res.redirect(303, '/newsletter/archieve');
+        }
+        if (req.xhr) return res.json({ success: true });
+        req.session.flash = {
+            type: 'success',
+            intro: 'thank you',
+            message: 'You have now been signed up for the newsletter.',
+        };
+        return res.redirect(303, '/newsletter/archieve');
+    });
+});
+
+app.post('/process', function (req, res) {
+    if (req.xhr || req.accepts('json,html') === 'json') {
+        res.send({ success: true });
+    } else {
+        res.redirect(303, '/thank-you');
+    }
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// TESTING FILE UPLOADING
+///////////////////////////////////////////////////////////////////////////////////////////////////
+app.get('/contest/vacation-photo', function (req, res) {
+    var now = new Date();
+    res.render('contest/vacation-photo', {
+        year: now.getFullYear(), month: now.getMonth()
+    });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        if (err) return res.redirect(303, '/error');
+        console.log('recieved fields:');
+        console.log(fields);
+        console.log('recieved files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+app.get('/headers', function (req, res) {
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for (var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    res.send(s);
+});
+
+app.get('/j-test', function (req, res) {
+    res.render('j-test');
+});
+
+app.get('/nursery-rhyme', function (req, res) {
+    res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function (req, res) {
+    res.json({
+        animal: 'squirrel',
+        bodyPart: 'tail',
+        adjective: 'bushy',
+        noun: 'heck',
+    });
+});
+
 app.get('/', function (req, res) {
     res.render('home');
+    //res.cookie('monster', 'nom nom');
+    //NOTE: Must have different name
+    //res.cookie('signed_monster', 'nom nom', { signed: true });
+
+    //NOTE: returns Cookie that was place beforehand
+   // var monster = req.cookies.monster;
+    //console.log(monster);
+    //res.clearCookie('monster');
+
 });
 app.get('/about', function (req, res) {
     res.render('about', {
         fortune: fortune.getFortune(),
         pageTestScript: '/qa/tests-about.js'
     });
+});
+
+app.get('/tours/hood-river', function (req, res) {
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function (req, res) {
+    res.render('tours/request-group-rate');
 });
 
 // catch 404 and forward to error handler
